b54055baf84f935f49f4a2b585508ed4ddc6cbcc
Rajesh <talaganarajesh@gmail.com>
2024-11-14 23:01:19 +0530

    asdfdsfsdf

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,15 @@ const WebSocket = require('ws');
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://locshare-five-blue.vercel.app/"],
+  methods: ["POST", "GET"],
+  credentials: true
+}));
+
+app.use(express.json());
+
+
 app.use(express.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/locationApp', {
