6842f746b56e23c8a0789f5010a28935e3c7cd45
Rajesh <talaganarajesh@gmail.com>
2024-11-15 12:33:01 +0530

    asdfsda

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const WebSocket = require('ws');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT|| 5000;
 
 app.use(cors({
   origin: "https://locshare-frontend.vercel.app",
